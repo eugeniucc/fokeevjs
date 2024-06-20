dce1e894c173b2cf717cb923ce19e7bd69de6306
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-06-20 14:45:20 +0300

    save

---
+++ js-6/js/js75-78.js
@@ -0,0 +1,21 @@
+// "use strict";
+
+// const airpline = new String("TAP Air Portugal");
+// const plane = "A430";
+
+// console.log(typeof airpline);
+
+// console.log(airpline[0]);
+// console.log(airpline.length);
+// console.log(airpline.indexOf("A"));
+// console.log(airpline.slice(4, 7));
+// console.log(airpline.slice(airpline.lastIndexOf(" ")));
+
+// const checkSeat = function (seat) {
+//   const s = seat[0];
+//   if (s == "A") console.log("Ваше место у окна");
+//   else console.log("Ваше место не у окна");
+// };
+
+// checkSeat(plane);
+
