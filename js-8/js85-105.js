fc99f5daeb4f1fa225df1e20fff7f40ff817b7db
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-06-23 12:03:23 +0300

    save

---
+++ js-8/js85-105.js
@@ -0,0 +1,41 @@
+// // // const arr2 = ["d", "e"];
+// // // console.log([...arr1, ...arr2]);
+// // // console.log(arr1);
+// // // console.log(arr2);
+
+// // "use strict";
+
+// // const arr1 = ["a", "b", "c", "d", "e"];
+
+// // // for (const value of arr1) {
+// // //   console.log(value);
+// // // }
+
+// // arr1.forEach(function (value, key, arr) {
+// //   console.log(`Ключ ${key}, значение ${value}, массив ${arr }`);
+// // });
+
+// "use strict";
+
+// const map = new Map([
+//   ["USD", "Dollars"],
+//   ["RUB", "Rubbles"],
+//   ["EUR", "Euro"],
+// ]);
+
+// // map.forEach(function (value, key, map) {
+// //   console.log(value);
+// //   console.log(key);
+// //   console.log(map);
+// // });
+
+// const set = new Set(map);
+
+// set.forEach(function (value, key, set) {
+//   console.log(key);
+//   console.log(value);
+//   console.log(set);
+// });
+
+
+
