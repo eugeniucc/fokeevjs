a33c1455c7b72c9da683d4843c732ac6e385d5d7
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-06-23 22:03:34 +0300

    save

---
+++ js-9/js.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// console.log(+"2323");
+
+// const str1 = "12px";
+// const str2 = "12.2px";
+
+// console.log(parseInt(str1));
+// console.log(parseFloat(str2));
+
+// console.log(Math.max(23, 44, 55));
+// console.log(Math.min(11, 22, 33));
+
+// function calcRandom(min, max) {
+//   return Math.trunc(Math.random() * (max - min) + 1) + min;
+// }
+
+// console.log(calcRandom(10, 20));
+
+// console.log(Math.round(2.4));
+// console.log(Math.round(2.5));
+
+console.log(234234234234234n + 22n);
