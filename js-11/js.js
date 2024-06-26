36c2b7854766639cff016294682b84d15d62ad9d
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-06-26 20:55:39 +0300

    save

---
+++ js-11/js.js
@@ -0,0 +1,45 @@
+// "use strict";
+
+// function User(firstName, lastName, age) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+// }
+
+// User.prototype.calcAge = function () {
+//   return 2037 - this.age;
+// };
+
+// const ivan = new User("Ivan", "Avdeev", 2002);
+// console.log(ivan);
+// console.log(ivan.calcAge());
+
+// const arr = [];
+// console.log(arr);
+
+// "use strict";
+
+// function Animal(name) {
+//   this.name = name;
+// }
+
+// Animal.prototype.speak = function () {
+//   console.log(`${this.name} makes a noise.`);
+// };
+
+// const dog = new Animal("Dog");
+// dog.speak;
+
+// const animal = {
+//   speak() {
+//     console.log(`${this.name} make a noise`);
+//   },
+// };
+
+// const dog = {
+//   name: "Dog",
+// };
+
+// dog.__proto__ = animal;
+
+// dog.speak();
